4dde7223bc6832cc9d77068dcc921e4e302fa9d0
kasif1234 <rebornland619@gmail.com>
2025-12-23 15:54:39 +0300

    KatrutsubaMultiply

---
+++ Divide and Conquer, Sorting and Searching, and Randomized Algorithms [15 hr]/Katrutsuba_Multiplication.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+// Katrutsuba Multiplication implementation goes here
+// Get foumula to split numbers and combine results
+
+int KatrutsubaMultiply(int x, int y){
+    
+
+    return x*y;
+}
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Katrutsuba Multiplication of 1234 and 5678: " << KatrutsubaMultiply(1234, 5678) << std::endl;
+    return 0;
+}
